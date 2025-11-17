68b386372a5483740d678b5de307689ac3ec334c
Petr Doroshev <doroshevpetr@gmail.com>
2025-11-17 13:17:02 +0300

    node implemetation added

--- src/dendrogram/Node.h
+++ src/dendrogram/Node.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <vector>
 #include <memory>
+#include <optional>
 #include "../DataPoint.h"
 
 class Node {
@@ -11,12 +12,21 @@ class Node {
 public:
 
     Node(size_t id, std::shared_ptr<DataPoint> point);
-    Node(std::shared_ptr<Node> left, std::shared_ptr<Node> right, size_t level);
+    Node(size_t id, std::shared_ptr<Node> left, std::shared_ptr<Node> right, size_t level);
+
+    void SetID(size_t id);
+    size_t GetID() const;
+
+    void SetLevel(size_t level);
+    size_t GetLevel() const;
+
+    void SetMergeValue(double mergeValue);
+    std::optional<double> GetMergeValue() const;
 
 
 private:
 
-    double merge_value;
+    std::optional<double> merge_value;
     size_t id;
     size_t level;
 
